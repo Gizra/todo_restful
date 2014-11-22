42b75e772b46a1fbac98b0c92a477f28cae1a810
Amitai Burstein <amitai@gizra.com>
2014-11-22 22:06:40 +0200

    Fix create

--- todo/modules/custom/todo_restful/plugins/restful/node/todos/1.0/TodosResource.class.php
+++ todo/modules/custom/todo_restful/plugins/restful/node/todos/1.0/TodosResource.class.php
@@ -14,6 +14,13 @@ class TodosResource extends \RestfulEntityBaseNode {
   public function publicFieldsInfo() {
     $public_fields = parent::publicFieldsInfo();
 
+    $public_fields['title'] = $public_fields['label'];
+    unset($public_fields['label']);
+
+    $public_fields['completed'] = array(
+      'property' => 'field_completed',
+    );
+
     return $public_fields;
   }
 }
